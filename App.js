b120a547e4d63c72e680af604ddc3a7bd31b5120
18wdwu07 <40220616+18wdwu07@users.noreply.github.com>
2018-08-30 08:24:03 +1200

    ex2

--- App.js
+++ App.js
@@ -1,36 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Person from './Person/Person';
+const welcome="Welcome to React"
 
 class App extends Component {
-
   render() {
-    
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-
-         /*<Person/>
-          <Person/>
-          <Person/>*/
-         // <button> Switch Name </button>
-          <Person name="David" age="28"/>
-          <Person name="Daniel" age="35"/>
-          
-            <Person name="Samson" age="18"> My Hobbies:Racing </Person>
-            <p className="Max"> Age:30 </p> 
-            </div>
-      //1.    return React.createElement('div',null, 'h1','Hi,I am a React App' );
-
-      /// 2.   return React.createElement('div',null, React.createElement('h1',null,'Hi,I am a React App'));
-        
-      ///3. return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi,I am a React App'));
-    )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title"> {welcome}</h1>
+        </header>
+        <p className="App-intro">
+          <Welcome/>
+        </p>
+      </div>
+    );
   }
 }
-
+class Welcome extends Component{
+  render(){
+    return (
+      <p> {welcome} </p>
+      );
+  }
+}
 export default App;
