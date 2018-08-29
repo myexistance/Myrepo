855569ff7d26905c4fb0154bcced32007b471616
18wdwu07 <40220616+18wdwu07@users.noreply.github.com>
2018-08-30 08:23:19 +1200

    ex1

--- App.js
+++ App.js
@@ -3,33 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-const message= "Welcome messge by ReactJS";
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Learning React JS</h1>
-        </header>
-        <p className="App-intro">
-         
-         {message}
-        </p>
-        <Person name="David"  age="20"/>
-        <Person  name="Daniel" age="30"/>
-        <Person name="Samson" age="25"> My hobby is Racing </Person>
 
-      </div>
-    );
-    //return React.createElement('div',null,React.createElement('h1',null,'Welcome by REACTJS'));
+  render() {
+    
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+
+         /*<Person/>
+          <Person/>
+          <Person/>*/
+         // <button> Switch Name </button>
+          <Person name="David" age="28"/>
+          <Person name="Daniel" age="35"/>
+          
+            <Person name="Samson" age="18"> My Hobbies:Racing </Person>
+            <p className="Max"> Age:30 </p> 
+            </div>
+      //1.    return React.createElement('div',null, 'h1','Hi,I am a React App' );
+
+      /// 2.   return React.createElement('div',null, React.createElement('h1',null,'Hi,I am a React App'));
+        
+      ///3. return React.createElement('div',{className:'App'}, React.createElement('h1',null,'Hi,I am a React App'));
+    )
   }
 }
-/*class Welcome extends App{
-  render(){
-    return(
-     <p> {message} Welcome to learn React JS </p>
-      )
-  }
-}*/
+
 export default App;
--- Person/Person.js
+++ Person/Person.js
@@ -1,10 +1,16 @@
 import React from 'react';
-const person = (props)=>{
-return (
-    <div>
-      <p> Hi, I am {props.name}. I am {props.age} years old</p> 
-       <p> {props.children} </p>
-       </div>
-       );
+//const person= () => { //function
+const person= (props) => { //function
+	//return <p> I am a person. I am Math.floor(Math.random()*30) years old!</p>
+	//return <p> I am a person. I am {Math.floor(Math.random()*30)} years old!</p>
+	//return <p> I am a {props.name}! I am {props.age} years old!</p>
+	return (
+		<div>
+		<p> I am {props.name}! I am {props.age} years old!</p>
+		<p> {props.children} </p>
+		</div>
+		)
+
 };
+
 export default person;
